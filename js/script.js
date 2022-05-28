32f9679c09272a4fc6d23cbda4b339ba31457885
sunairrajd <sunairrajd@gmail.com>
2022-05-29 00:55:24 +0530

    upload first time
    upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+let viewportHeight = window.innerHeight;
+let viewportWidth = window.innerWidth;
+console.log(viewportWidth)
+
+
+// $(document).ready(function(){
+//   $(window).bind('scroll', function() {
+//   var navHeight = $('viewportHeight').height() - 70;
+//
+//   console.log(navHeight);
+//     if ($(window).scrollTop() > navHeight) {
+//       $('nav').addClass('sticky-top');
+//       $('nav').removeClass('sticky-top');
+//     }
+//     else {
+//       $('nav').removeClass('sticky-top');
+//         $('nav').addClass('sticky-top');
+//
+//     }
+//  });
+// });
